c327373d5c1f50e057846ae047ca6e0d87ee13ea
Amr Fawzy <afawzy@dr.com>
2017-07-30 01:41:03 +0200

    Add files via upload
    Adding GUI Structure to the trunk. The GUI XML will be developed with glade tool.

---
+++ GUI/example.c
@@ -0,0 +1,29 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[])
+{
+	GtkBuilder *gtkBuilder;
+	GtkWidget *window;
+	GError     *error = NULL;
+	gtk_init(&argc, &argv);
+	    
+	gtkBuilder = gtk_builder_new();
+	if(!gtk_builder_add_from_file(gtkBuilder, "builder.ui", &error))
+	{
+		g_warning( "%s", error->message );
+        g_free( error );
+        return( 1 );
+	}
+	window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "CRC_Configuration"));
+	
+	 /* Connect signals */
+    gtk_builder_connect_signals( gtkBuilder, NULL );
+	
+	 /* Destroy builder, since we don't need it anymore */   
+	g_object_unref(G_OBJECT(gtkBuilder));
+	
+	gtk_widget_show(window);
+	gtk_main();
+    
+    return 0;
+}
