85eab28f37d48f0a07d3e3164447fd0bcafd143c
Amr Fawzy <afawzy@dr.com>
2017-08-08 17:49:07 +0200

    fixing the batch file of the build system to be universal.

--- build.bat
+++ build.bat
@@ -5,15 +5,21 @@ set MAKE_PATH=%~dp0Tools\Make
 set PATH=%PATH%;%CMAKE_PATH%;%MAKE_PATH%
 IF EXIST Builds GOTO start_build
 mkdir Builds
+cd Builds
+cmake .. -G "MinGW Makefiles"
+cmake .. -G "MinGW Makefiles"
+call mingw32-make all -j4
+GOTO :Test_output
+
 :start_build
 IF EXIST .\Builds\bin\bin.exe (
 	del .\Builds\bin\bin.exe
 	)
 cd Builds
 cmake .. -G "MinGW Makefiles"
-cmake .. -G "MinGW Makefiles"
 call mingw32-make all -j4
 
+:Test_output
 IF EXIST .\bin\bin.exe (
 	echo ****************************
 	echo * Build Successfull *
